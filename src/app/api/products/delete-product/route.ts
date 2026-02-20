f155f997ef38c454b60cd1ad181862baf129d3e3
SangameshwarGaikwad21 <sangamgaikwad214@gmail.com>
2026-02-20 22:53:22 +0530

    added the delete product and update product

--- src/app/api/products/delete-product/route.ts
+++ src/app/api/products/delete-product/route.ts
@@ -3,9 +3,31 @@ import connectionToDB from "@/config/db";
 import Products from "@/models/Product.models";
 
 
-export async function DELETE(request:NextRequest) {
+export async function DELETE(request:NextRequest,{ params }: { params: { id: string } }) {
     try {
-        
+        await connectionToDB();
+        const { searchParams } = new URL(request.url);
+        const productId = searchParams.get("productId");
+        if(!productId){
+          return NextResponse.json(
+            {message:"Product ID required"},
+            {status:401}
+          )
+        }
+
+        const deleteProduct=await Products.findByIdAndDelete(productId)
+         if (!deleteProduct) {
+          return NextResponse.json(
+            { message: "Product not found" },
+            { status: 404 }
+          );
+        }
+
+        return NextResponse.json(
+          { message: "Product deleted successfully", deleteProduct },
+          { status: 200 }
+        );
+
     } 
     catch (error) {
     return NextResponse.json(
--- src/app/api/products/get-product/route.ts
+++ src/app/api/products/get-product/route.ts
@@ -1,15 +1,27 @@
 import { NextRequest, NextResponse } from "next/server";
-import { uploadImageToCloudinary } from "@/utils/uploadImage";
 import connectionToDB from "@/config/db";
 import Products from "@/models/Product.models";
 
 
 export async function GET(request:NextRequest) {
     try {
-        
+      await connectionToDB();
+      
+      const getProduct=await Products.find({})
+      if(!getProduct){
+        return NextResponse.json(
+          {message:"Product is not get"},
+          {status:401}
+        )
+      }
+
+      return NextResponse.json(
+        {message:"Product get Successfully",getProduct},
+        {status:200}
+      )
+
     } 
     catch (error) {
-
     return NextResponse.json(
       { message: "Not Get the product", error },
       { status: 500 }
